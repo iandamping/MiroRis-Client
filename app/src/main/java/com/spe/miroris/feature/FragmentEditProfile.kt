d13fe661c802022862364f158069ecd7bdc73e14
Ian Damping <ian.damping@spesolution.com>
2023-04-26 12:50:17 +0700

    add dialog on edit profile

--- app/src/main/java/com/spe/miroris/feature/FragmentEditProfile.kt
+++ app/src/main/java/com/spe/miroris/feature/FragmentEditProfile.kt
@@ -5,12 +5,24 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.content.res.AppCompatResources
+import coil.load
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.spe.miroris.R
 import com.spe.miroris.base.BaseFragmentDataBinding
+import com.spe.miroris.databinding.DialogSaveEditProfileBinding
 import com.spe.miroris.databinding.FragmentEditProfileBinding
+import com.spe.miroris.util.layoutInflater
 
 class FragmentEditProfile : BaseFragmentDataBinding<FragmentEditProfileBinding>(),
     AdapterView.OnItemSelectedListener {
+
+    private var _dialogEditProfileBinding: DialogSaveEditProfileBinding? = null
+    private val dialogEditProfileBinding get() = _dialogEditProfileBinding!!
+    private lateinit var alert: AlertDialog
+
+
     override val bindingInflater: (LayoutInflater, ViewGroup?, Boolean) -> FragmentEditProfileBinding
         get() = FragmentEditProfileBinding::inflate
 
@@ -33,6 +45,43 @@ class FragmentEditProfile : BaseFragmentDataBinding<FragmentEditProfileBinding>(
     }
 
     override fun viewCreated() {
+        setupDialogView()
+
+        binding.btnSaveChange.setOnClickListener {
+            if (::alert.isInitialized) {
+                //todo : manipulasi ini berdasarkan data dari service. ini hanya untuk melihat dialog nya saja
+                with(dialogEditProfileBinding) {
+                    ivEditProfile.load(
+                        AppCompatResources.getDrawable(
+                            this.root.context,
+                            R.drawable.img_failed_save
+                        )
+                    )
+                    tvEditProfileInfo.text = "Proses simpan gagal"
+                    btnOk.setOnClickListener {
+                        alert.dismiss()
+                    }
+                    ivCloseDialog.setOnClickListener {
+                        alert.dismiss()
+                    }
+                }
+                alert.show()
+            }
+        }
+    }
+
+    private fun setupDialogView() {
+        _dialogEditProfileBinding =
+            DialogSaveEditProfileBinding.inflate(requireContext().layoutInflater)
+
+        val materialDialogBuilder =
+            MaterialAlertDialogBuilder(requireContext(), R.style.MaterialAlertDialog_Round)
+                .setView(dialogEditProfileBinding.root)
+
+        alert = materialDialogBuilder.create().apply {
+            setCancelable(true)
+            setCanceledOnTouchOutside(true)
+        }
     }
 
     override fun onItemSelected(parent: AdapterView<*>, view: View?, pos: Int, id: Long) {
@@ -45,5 +94,9 @@ class FragmentEditProfile : BaseFragmentDataBinding<FragmentEditProfileBinding>(
         // Another interface callback
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _dialogEditProfileBinding = null
+    }
 
 }
